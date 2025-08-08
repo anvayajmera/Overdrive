f0df95f4ee06ca026c389e9b67daf2fead4aee39
Bryan Zou <26brzou@gmail.com>
2025-08-08 19:45:29 -0400

    First test of camera through opencv

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
 import cv2
 
+camera1 = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+while camera1.isOpened():
+    # ret indicates available frame
+    ret, frame = camera1.read()
+    if ret:
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+camera1.release()
+cv2.destroyAllWindows()
