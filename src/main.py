a57dcdbb449f3f155e0dc9bd401c04113fba7f83
megalithium20 <megalithium2.0@gmail.com>
2025-08-11 19:59:45 -0400

    Finally fixed imports with poetry and added .whl file for opencv with gstreamer support on linux. Also added model files to repo.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
+import numpy as np
 import cv2
+from jetcam.csi_camera import CSICamera
 
-camera1 = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+camera1 = CSICamera(width=960, height=540, capture_width=1920, capture_height=1080, capture_fps=30)
 
-while camera1.isOpened():
-    # ret indicates available frame
-    ret, frame = camera1.read()
-    if ret:
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
-    else:
+camera1.running = True
+
+window_name = "JetCam Video"
+cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+
+while camera1.running:
+
+    # fetch numpy frame from cam
+    frame = camera1.value
+
+    cv2.imshow(window_name, frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-camera1.release()
+    if cv2.getWindowProperty(window_name, cv2.WND_PROP_AUTOSIZE) < 1:
+        break
+
+camera1.running = False
 cv2.destroyAllWindows()
