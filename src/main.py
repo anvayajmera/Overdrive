1c9a146393119c9e51f27818222a004407d6116c
Mega Lithium <megalithium2.0@gmail.com>
2026-02-20 22:08:17 -0500

    Added in logic for X11 sharing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import cv2, time, os
 from classes.Robot import Robot
 from classes.linetrace import linetrace, init
-from classes.constants import GUI, TIMESTEP, FPS
+from classes.constants import GUI, TIMESTEP, X11
 
-if GUI:
+if GUI and not X11:
     os.environ.setdefault("DISPLAY", ":0")
 
 r = Robot()
@@ -16,10 +16,10 @@ print("TIMESTEP: ", TIMESTEP)
 
 try:
     while True:
-        # now = time.perf_counter()
-        # if now < next_t:
-        #     time.sleep(next_t - now)
-        # next_t += TIMESTEP
+        now = time.perf_counter()
+        if now < next_t:
+            time.sleep(next_t - now)
+        next_t += TIMESTEP
 
         linetrace()
 
