1c7513dd62428f5156fa9446a6ab5399a1e06428
Bryan Zou <26brzou@gmail.com>
2026-03-17 20:02:14 -0400

    task: fixed a small issue causing cameras to crash when stoppin ghte code

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ import cv2
 from classes.Robot import Robot
 from constants import GUI, TIMESTEP, X11
 from linetrace.linetrace import init, linetrace
-from green_square.green_square import green_square
 
 if GUI and not X11:
     os.environ.setdefault("DISPLAY", ":0")
@@ -29,19 +28,18 @@ try:
         if now < next_t:
             time.sleep(next_t - now)
         next_t += TIMESTEP
-        r.update()
 
         # r.turnLeft()
 
         # linetrace()
-
-        green_square()
 
         if GUI:
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
 except KeyboardInterrupt:
     print("Stopping bc of ctrl + C.")
-    r.stop()
 
-r.cleanup()
+try:
+    r.stop()
+finally:
+    r.cleanup()
